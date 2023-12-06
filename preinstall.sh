d4c308add8c54d7a2eb0cc9b2a812adbe92c7d0b
Dominik Filip <dominik.filip@netguru.com>
2023-12-06 21:26:04 +0100

    add ci config

---
+++ preinstall.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+NOCOLOR='\033[0m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+
+# Install i18n packages
+yarn add i18next react-i18next i18next-parser i18next-browser-languagedetector i18next-http-backend
+
+# Check if installation was successful
+if [ $? -eq 0 ]; then
+  echo -e "${GREEN}Install was successful.${NOCOLOR}"
+else
+  echo -e "${RED} Error during installation${NOCOLOR}".
+fi
+
+# Move locales folder to src
+source_folder="locales/"
+target_folder="src/"
+
+if [ -d "$source_folder" ]; then
+  # Przeniesienie pliku do docelowego folderu
+  mv "$source_folder" "$target_folder"
+  echo -e "${GREEN}Locales folder moved successfully to $target_folder.${NOCOLOR}"
+else
+  echo -e "${RED}Source folder does not exist.${NOCOLOR}"
+fi
--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {useTranslation} from "react-i18next";
 
 function App() {
   const [count, setCount] = useState(0)
 
+    const { t,  } = useTranslation();
+    document.title = t("title");
+    console.log(t("title"));
   return (
     <>
       <div>
@@ -26,7 +30,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        AWS Amplify + Vite + React + TypeScript + Tailwind CSS + i18n
+          {t('title')}
       </p>
     </>
   )
