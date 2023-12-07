30cc2b4ca3cb64aed2ddb2e66eef218d0372397f
Dominik Filip <dominik.filip@netguru.com>
2023-12-07 11:19:32 +0100

    add ci config

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {useTranslation} from "react-i18next";
+import {Card} from "./Card.tsx";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -36,6 +37,7 @@ function App() {
         </button>
           <button onClick={changeLng}>DASDA</button>
         <p>
+            <Card/>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
